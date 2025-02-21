dc861c8a7e51cacc4045e82a0e6778e076fc2cea
navidio.h <navidppanahi@gmail.com>
2025-02-21 17:54:42 +0330

    Add Fund data model class

---
+++ memeCoinWebApp/Models/Fund.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace memeCoinWebApp.Models;
+
+public class Fund
+{
+	[Key]
+	public int Id { set; get; }
+	public int Amount { set; get; }
+	public DateTime Timestamp { set; get; }
+
+	// foreign keys
+	public int UserId { set; get; }
+	public User User { set; get; }
+}

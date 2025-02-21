bfd61f692c43b7beafa155a5448112d4f4a15fb4
Arsham <Arsham2002>
2025-02-21 13:44:13 +0330

    Create MVC files and Add gitignore file

---
+++ memeCoinWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace memeCoinWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

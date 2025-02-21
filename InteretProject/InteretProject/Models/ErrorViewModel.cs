012242d6a955b679369d69314b5bc71032fc53a8
Arsham <Arsham2002>
2025-02-21 13:19:49 +0330

    Delete files for recreate

--- InteretProject/InteretProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace InteretProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

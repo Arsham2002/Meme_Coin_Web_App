a2a08f8e7bcc353f5ade1a9658ca887f713e64a5
navidio.h <navidppanahi@gmail.com>
2025-03-03 20:58:03 +0330

    Fix foreign keys in Transfer, Fund, and Message data model classes

---
+++ memeCoinWebApp/Migrations/20250303172307_ChangeUserID.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace memeCoinWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Transfer");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Message");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Fund");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Transfer",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Message",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Fund",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
